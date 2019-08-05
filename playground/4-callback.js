caab50ea49c2c454cffadff9ac17efb740296877
irodriguez93 <nerockjr@yahoo.com>
2019-08-05 09:17:24 -0500

    Callback funcstion

---
+++ playground/4-callback.js
@@ -0,0 +1,25 @@
+
+    setTimeout(()=>{
+        console.log('Two seconds are up')
+    }, 2000)
+
+const names = ['Andrew', 'Jen', 'Jess']
+const shortNames = names.filter((name) => {
+    return name.length <=4
+})
+
+const geocode = (address, callback) => {
+    setTimeout(()=>{
+        const data = {
+            latitude: 0,
+            longitude: 0
+        }
+        callback(data)
+
+    }, 2000)
+}
+
+geocode('Philcadelphia', (data) => {
+    console.log(data)
+})
+
--- weather-app/app.js
+++ weather-app/app.js
@@ -1,25 +1,46 @@
 const request = require('request')
 
 
-const url = 'https://api.darksky.net/forecast/18c9fed0acff8ae6fd162ad43a718bf7/37.8267,-122.4233'
+const url = 'https://api.darksky.net/forecast/18c9fed0acff8ae6fd162ad43a718bf7/37.8267,43.2534'
 
-request({url: url, json: true}, (error, response)=> {
-    //const data = response.body
-    const rainProbability = response.body.currently.precipProbability
-    const degreesCurrent = response.body.currently.temperature
-    const dailySummary = response.body.daily.data[0].summary
-    console.log(dailySummary + ' It is currently ' + degreesCurrent +' degrees out. There is a ' + rainProbability + '% chance of rain')
-})
+// request({url: url, json: true}, (error, response)=> {
+//     if (error){
+//         console.log('unable to connect to weather service!')
+//     }else if (response.body.error) {
+//         console.log('unable to find location')
+
+//     }else{
+//         const rainProbability = response.body.currently.precipProbability
+//         const degreesCurrent = response.body.currently.temperature
+//         const dailySummary = response.body.daily.data[0].summary
+//         console.log(dailySummary + ' It is currently ' + degreesCurrent +' degrees out. There is a ' + rainProbability + '% chance of rain')
+//     }
+
+// })
 
 //Geocoding
+
+// https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?
+//access_token=pk.eyJ1IjoiaXJvZHJpZ3VlejkzIiwiYSI6ImNqeXcwcDAybTBjNWozaGxnNTQwcG16eGUifQ.v4fED-g0CJb_l3TKS-YUXg
+
+
 
 const geocodeURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?'
 const keyGeoAPI = 'access_token=pk.eyJ1IjoiaXJvZHJpZ3VlejkzIiwiYSI6ImNqeXcwcDAybTBjNWozaGxnNTQwcG16eGUifQ.v4fED-g0CJb_l3TKS-YUXg'
 const gurl=geocodeURL+keyGeoAPI
 
 request({url: gurl, json: true}, (error, response)=>{
-    console.log(gurl)
-    console.log('lattitude is: ' + response.body.features[0].center[1])
-    debugger
-    console.log('longitude is: ' + response.body.features[0].center[0])
+
+    if (error){
+        console.log('Unable to connect to service!')
+    } else if (response.body.message==='Not Authorized - Invalid Token') {
+        console.log('Not Authorized - Invalid Token')
+
+    }else if (response.body.features === undefined || response.body.features.length == 0){
+        console.log('unable to find the correct location')
+    }else{
+        console.log('lattitude is: ' + response.body.features[0].center[1])
+        debugger
+        console.log('longitude is: ' + response.body.features[0].center[0])
+     }
 })
