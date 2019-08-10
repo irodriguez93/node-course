af1838005237b55beb43b21d524105c1d0ae41ff
irodriguez93 <nerockjr@yahoo.com>
2019-08-10 17:31:00 -0500

    Update 4-callback.js

--- playground/4-callback.js
+++ playground/4-callback.js
@@ -1,25 +1,38 @@
 
+const add =  (x, y, callback) => {
     setTimeout(()=>{
-        console.log('Two seconds are up')
-    }, 2000)
-
-const names = ['Andrew', 'Jen', 'Jess']
-const shortNames = names.filter((name) => {
-    return name.length <=4
-})
-
-const geocode = (address, callback) => {
-    setTimeout(()=>{
-        const data = {
-            latitude: 0,
-            longitude: 0
-        }
-        callback(data)
-
+        const sum = x+y
+        callback(sum)
     }, 2000)
 }
 
-geocode('Philcadelphia', (data) => {
-    console.log(data)
+add (1,4,(sum)=>{
+    console.log(sum)
 })
+
+
+
+//     setTimeout(()=>{
+//         console.log('Two seconds are up')
+//     }, 2000)
+
+// const names = ['Andrew', 'Jen', 'Jess']
+// const shortNames = names.filter((name) => {
+//     return name.length <=4
+// })
+
+// const geocode = (address, callback) => {
+//     setTimeout(()=>{
+//         const data = {
+//             latitude: 0,
+//             longitude: 0
+//         }
+//         callback(data)
+
+//     }, 2000)
+// }
+
+// geocode('Philcadelphia', (data) => {
+//     console.log(data)
+// })
 
