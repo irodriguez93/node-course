52416e61c2f6d38ba84cb55b8ab5360a4e84397d
irodriguez93 <nerockjr@yahoo.com>
2019-08-11 11:01:52 -0500

    Update app.js

--- weather-app/app.js
+++ weather-app/app.js
@@ -2,11 +2,16 @@ const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
 geocode('Boston', (error, data)=>{
+    if (error){
+        return console.log(error)
+    }
     console.log('Error', error)
     console.log('Data', data)
+    forecast(data.lattitude,data.longitude, (error, forecastData) => {
+        if (error){
+            return console.log(error)
+        }
+        console.log(data.location)
+        console.log(forecastData)
+      })
 })
-
-forecast(-75.7088, 44.1545, (error, data) => {
-    console.log('Error', error)
-    console.log('Data', data)
-  })
