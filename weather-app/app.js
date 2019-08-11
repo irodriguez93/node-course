61b89342f2b389328a09e3d0f815fb13f31569dc
irodriguez93 <nerockjr@yahoo.com>
2019-08-10 23:36:07 -0500

    update

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,46 +1,36 @@
-const request = require('request')
 
-
-const url = 'https://api.darksky.net/forecast/18c9fed0acff8ae6fd162ad43a718bf7/37.8267,43.2534'
-
-// request({url: url, json: true}, (error, response)=> {
-//     if (error){
-//         console.log('unable to connect to weather service!')
-//     }else if (response.body.error) {
-//         console.log('unable to find location')
-
-//     }else{
-//         const rainProbability = response.body.currently.precipProbability
-//         const degreesCurrent = response.body.currently.temperature
-//         const dailySummary = response.body.daily.data[0].summary
-//         console.log(dailySummary + ' It is currently ' + degreesCurrent +' degrees out. There is a ' + rainProbability + '% chance of rain')
-//     }
-
-// })
-
-//Geocoding
-
-// https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?
-//access_token=pk.eyJ1IjoiaXJvZHJpZ3VlejkzIiwiYSI6ImNqeXcwcDAybTBjNWozaGxnNTQwcG16eGUifQ.v4fED-g0CJb_l3TKS-YUXg
+const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
 
 
 
-const geocodeURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?'
-const keyGeoAPI = 'access_token=pk.eyJ1IjoiaXJvZHJpZ3VlejkzIiwiYSI6ImNqeXcwcDAybTBjNWozaGxnNTQwcG16eGUifQ.v4fED-g0CJb_l3TKS-YUXg'
-const gurl=geocodeURL+keyGeoAPI
+// const geocode = (address, callback) => {
+//     const URL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' +encodeURIComponent(address) + '.json?access_token=pk.eyJ1IjoiaXJvZHJpZ3VlejkzIiwiYSI6ImNqeXcwcDAybTBjNWozaGxnNTQwcG16eGUifQ.v4fED-g0CJb_l3TKS-YUXg'
+//     request({url: URL, json: true}, (error, response)=>{
+//         if (error){
+//             callback('unable to connnect to location services')
+//         } else if (response.body.message==='Not Authorized - Invalid Token') {
+//             callback('Not Authorized - Invalid Token')
+    
+//         }else if (response.body.features === undefined || response.body.features.length == 0){
+//           callback('unable to find location')
+//         }else{
+//             callback(undefined,{
+//                 lattitude: response.body.features[0].center[1],
+//                 longitude: response.body.features[0].center[0],
+//                 location: response.body.features[0].place_name
+//             })
+//          }
 
-request({url: gurl, json: true}, (error, response)=>{
+//     })
+// }s
 
-    if (error){
-        console.log('Unable to connect to service!')
-    } else if (response.body.message==='Not Authorized - Invalid Token') {
-        console.log('Not Authorized - Invalid Token')
+forecast(-75.7088, 44.1545, (error, data) => {
+    console.log('Error', error)
+    console.log('Data', data)
+  })
 
-    }else if (response.body.features === undefined || response.body.features.length == 0){
-        console.log('unable to find the correct location')
-    }else{
-        console.log('lattitude is: ' + response.body.features[0].center[1])
-        debugger
-        console.log('longitude is: ' + response.body.features[0].center[0])
-     }
+geocode('Boston', (error, data)=>{
+    console.log('Error', error)
+    console.log('Data', data)
 })
---
+++ weather-app/utils/forecast.js
@@ -0,0 +1,24 @@
+const request =  require('request')
+
+
+const forecast = (x,y, callback )=> {
+    const url = 'https://api.darksky.net/forecast/18c9fed0acff8ae6fd162ad43a718bf7/'+ x +','+ y 
+    console.log(url)
+
+    request({url: url, json:true}, (error, response)=>{
+        if (error){
+            callback('unable to connect to weather service!', undefined)
+        }else if (response.body.error) {
+            callback('unable to find location', undefined)
+
+        }else{
+            const rainProbability = response.body.currently.precipProbability
+            const degreesCurrent = response.body.currently.temperature
+            const dailySummary = response.body.daily.data[0].summary
+            callback(undefined, dailySummary + ' It is currently ' + degreesCurrent +' degrees out. There is a ' + rainProbability + '% chance of rain')
+        }
+
+    })
+}
+
+module.exports = forecast
